144eef4d36d8f11aaa1dc94a1f4b18217b7278ff
Angelo Guiao <angeloguiao.gci@gmail.com>
2024-10-21 02:07:13 +0800

    final

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -8,13 +8,29 @@ import cover3 from "@/assets/cover-3.png"
 import cover4 from "@/assets/cover-4.png"
 import cover5 from "@/assets/cover-5.png"
 import cover6 from "@/assets/cover-6.png"
-import cover7 from "@/assets/cover-6.png"
+import cover7 from "@/assets/cover-7.png"
+import cover8 from "@/assets/cover-8.png"
+import cover9 from "@/assets/cover-9.png"
+import cover10 from "@/assets/cover-10.png"
+import cover11 from "@/assets/cover-11.png"
+import cover12 from "@/assets/cover-12.png"
+import cover13 from "@/assets/cover-13.png"
+import cover14 from "@/assets/cover-14.png"
+import cover15 from "@/assets/cover-15.png"
 import wholecover1 from "@/assets/whole-cover-1.png"
 import wholecover2 from "@/assets/whole-cover-2.png"
 import wholecover3 from "@/assets/whole-cover-3.png"
 import wholecover4 from "@/assets/whole-cover-4.png"
 import wholecover5 from "@/assets/whole-cover-5.png"
 import wholecover6 from "@/assets/whole-cover-6.png"
+import wholecover7 from "@/assets/whole-cover-7.png"
+import wholecover8 from "@/assets/whole-cover-8.png"
+import wholecover9 from "@/assets/whole-cover-9.png"
+import wholecover10 from "@/assets/whole-cover-10.png"
+import wholecover11 from "@/assets/whole-cover-11.png"
+import wholecover12 from "@/assets/whole-cover-12.png"
+import wholecover13 from "@/assets/whole-cover-13.png"
+import wholecover14 from "@/assets/whole-cover-14.png"
 import tool1 from "@/assets/tool-1.png"
 import tool2 from "@/assets/tool-2.png"
 import tool3 from "@/assets/tool-3.png"
@@ -33,12 +49,28 @@ const images = {
  cover5,
  cover6,
  cover7,
+ cover8,
+ cover9,
+ cover10,
+ cover11,
+ cover12,
+ cover13,
+ cover14,
+ cover15,
  wholecover1,
  wholecover2,
  wholecover3,
  wholecover4,
  wholecover5,
  wholecover6,
+ wholecover7,
+ wholecover8,
+ wholecover9,
+ wholecover10,
+ wholecover11,
+ wholecover12,
+ wholecover13,
+ wholecover14,
  tool1,
  tool2,
  tool3,
--- src/store/cardData.ts
+++ src/store/cardData.ts
@@ -39,11 +39,47 @@ export const cardData: CardData[] = [
     link: "https://ai-one-rust.vercel.app/",
   },
   {
+    title: "iPHONE 15 Pro",
+    description: "Apple Website Clone",
+    coverImage: images.cover11,
+    wholeCoverImage: images.wholecover10,
+    link: "https://3-d-iphone.vercel.app/",
+  },
+
+  {
     title: "HiLink Travel",
     description: "Travel Companion",
     coverImage: images.cover6,
     wholeCoverImage: images.wholecover5,
     link: "https://travel-iota-ten.vercel.app/",
+  },
+  {
+    title: "3D Portfolio",
+    description: "Learning threeJS",
+    coverImage: images.cover9,
+    wholeCoverImage: images.wholecover8,
+    link: "  https://3d-portfolio-gules.vercel.app/",
+  },
+  {
+    title: "CSE Study Corner",
+    description: "Host, Connect, Celebrate",
+    coverImage: images.cover15,
+    wholeCoverImage: images.wholecover14,
+    link: "https://study-corner-es9e.vercel.app/",
+  },
+  {
+    title: "Tech Innovations",
+    description: "Exploring technology",
+    coverImage: images.cover10,
+    wholeCoverImage: images.wholecover9,
+    link: "  https://tech-innovations.vercel.app/",
+  },
+  {
+    title: "St. Joseph School",
+    description: "School Website",
+    coverImage: images.cover8,
+    wholeCoverImage: images.wholecover7,
+    link: "  https://saintjoseph.vercel.app/",
   },
   {
     title: "The18 Manager",
@@ -52,5 +88,34 @@ export const cardData: CardData[] = [
     wholeCoverImage: images.wholecover6,
     link: "https://the18.vercel.app/",
   },
+  {
+    title: "Howard University",
+    description: "School Website",
+    coverImage: images.cover12,
+    wholeCoverImage: images.wholecover11,
+    link: "https://howard-university-r5rq.vercel.app/",
+  },
+
+  {
+    title: "Community Church",
+    description: "Our Church Website",
+    coverImage: images.cover13,
+    wholeCoverImage: images.wholecover12,
+    link: "https://clhcc.vercel.app/",
+  },
+
+  {
+    title: "Evently Connect",
+    description: "Host, Connect, Celebrate",
+    coverImage: images.cover14,
+    wholeCoverImage: images.wholecover13,
+    link: "https://evently-qstz.vercel.app/",
+  },
+
+
+
+
+
+  
 ];
 
